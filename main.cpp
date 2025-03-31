4fce843f9b039c23995ce99bc13db4a79a2d9f19
Janet <jhwilks@hotmail.com>
2025-03-31 21:22:43 +0100

    string, cin, cout, endl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <istream>
+#include <string>
 
 
 int main() {
-    std::cout << "Hello World!";
+
+    std::string choice;
+
+    std::cout << "What direction next? " << std::endl;
+    std::cin >> choice;
+    std::cout << choice << std::endl;
 }
