d26f4e73968ef02790ce6126afe0856564a6c402
Janet <jhwilks@hotmail.com>
2025-04-13 18:14:49 +0100

    Updated getter and setter names in Thing and Room Classes to have different method names. Updated tests on Room object in test_main.cpp to test getter and setter.

--- cpp_adventure/src/classes/thing.cpp
+++ cpp_adventure/src/classes/thing.cpp
@@ -5,12 +5,12 @@
 // Constructor with initializer list
 // const ensures immutability
 // string& name passes by reference for efficiency
-Thing::Thing(const std::string &name) : _ObjectName(name) {};
+Thing::Thing(const std::string &name) : _gameObjectName(name) {};
 
 // Get name method
-const std::string &Thing::getName() const { return _ObjectName; }
+const std::string &Thing::getGameObjectName() const { return _gameObjectName; }
 
 // Set name method
-void Thing::setName(const std::string &newName) { _ObjectName = newName; } 
+void Thing::setGameObjectName(const std::string &newName) { _gameObjectName = newName; } 
 
 // The virtual (abstract) method does not need to be defined because it will be overridden by the (derived) Room class
