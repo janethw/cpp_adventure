766cea2df3c50c7bc65efe5f8944ae308cdf52ed
Janet <jhwilks@hotmail.com>
2025-04-14 22:21:50 +0100

    Added an init function (.h and .cpp). Amended main.cpp to call the init function. Updated CMakeLists.txt at root level to add the init.cpp function to the variable SRC_FUNCTIONS

---
+++ cpp_adventure/src/functions/init.cpp
@@ -0,0 +1,17 @@
+
+#include "init.h"
+#include "../classes/room.h"
+
+void init() {
+    // r2
+    // |
+    // r1 - r3
+    // |    |
+    // r4 - r5
+
+    Room r1("Room", "Hall", 2, 3, 4, -1);
+    Room r2("Room", "Cave", -1, -1, 1, -1);
+    Room r3("Room", "Castle", -1, -1, 5, 1);
+    Room r4("Room", "Market", 1, 5, -1, -1);
+    Room r5("Room", "Garden", 3, -1, -1, 4);
+}
--- cpp_adventure/src/main.cpp
+++ cpp_adventure/src/main.cpp
@@ -5,21 +5,23 @@
 #include "classes/room.h"
 #include "classes/thing.h"
 #include "functions/enterRoom.h"
+#include "functions/init.h"
 
 
 int main() {
 
-    Room hall("Room", "Hall", 2, 3, 4, -1);
-    Room cave("Room", "Cave", -1, -1, 1, -1);
-    Room castle("Room", "Castle", -1, -1, 5, 1);
-    Room market("Room", "Market", 1, 5, -1, -1);
-    Room garden("Room", "Garden", 3, -1, -1, 4);
+    // Room hall("Room", "Hall", 2, 3, 4, -1);
+    // Room cave("Room", "Cave", -1, -1, 1, -1);
+    // Room castle("Room", "Castle", -1, -1, 5, 1);
+    // Room market("Room", "Market", 1, 5, -1, -1);
+    // Room garden("Room", "Garden", 3, -1, -1, 4);
     
+    std::void init;
     std::string choice;
     std::string room;
     std::string message;
 
-    room = hall.getRoomName();
+    room = r1.getRoomName();
 
     message = enterRoom(room);
 
