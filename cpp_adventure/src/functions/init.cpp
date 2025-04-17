d3b30d93b73f0ce1ba39909ceb59c7f52dee1312
janethw <jhwilks@hotmail.com>
2025-04-17 11:22:42 +0200

    changed room object number to start from 0. Configuration works, but build failing on test_main.cpp

--- cpp_adventure/src/functions/init.cpp
+++ cpp_adventure/src/functions/init.cpp
@@ -4,15 +4,15 @@
 #include "../classes/room.h"
 
 void init() {
-    // r2
+    // r1
     // |
-    // r1 - r3
+    // r0 - r2
     // |    |
-    // r4 - r5
+    // r3 - r4
 
-    Room r1("Room", "Hall", 2, 3, 4, -1);
-    Room r2("Room", "Cave", -1, -1, 1, -1);
-    Room r3("Room", "Castle", -1, -1, 5, 1);
-    Room r4("Room", "Market", 1, 5, -1, -1);
-    Room r5("Room", "Garden", 3, -1, -1, 4);
+    Room r0("Room", "Hall", 2, 3, 4, -1);
+    Room r1("Room", "Cave", -1, -1, 1, -1);
+    Room r2("Room", "Castle", -1, -1, 5, 1);
+    Room r3("Room", "Market", 1, 5, -1, -1);
+    Room r4("Room", "Garden", 3, -1, -1, 4);
 }
