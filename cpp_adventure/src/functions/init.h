37eeacd5c280ca994e08486b2a59b718f4473001
janethw <jhwilks@hotmail.com>
2025-04-17 23:25:33 +0200

    change of init() function name to initRooms() and associated changes in functions dir and CMakeLists.txt files. Configures but doesn't build. Need to debug initRooms.cpp vector

--- cpp_adventure/src/functions/init.h
+++
@@ -1,7 +0,0 @@
-#ifndef __INIT_H_
-#define __INIT_H_
-
-void init();
-
-
-#endif // INIT
--- cpp_adventure/src/functions/initRooms.cpp
+++ cpp_adventure/src/functions/initRooms.cpp
@@ -1,9 +1,8 @@
-
 #include <vector>
-#include "init.h"
+#include "initRooms.h"
 #include "../classes/room.h"
 
-void init() {
+std::vector<Room> initRooms() {
     // r1
     // |
     // r0 - r2
@@ -15,4 +14,7 @@ void init() {
     Room r2("Room", "Castle", -1, -1, 5, 1);
     Room r3("Room", "Market", 1, 5, -1, -1);
     Room r4("Room", "Garden", 3, -1, -1, 4);
+
+    std::vector<Room> roomVec = {r0, r1, r2, r3, r4};
+    return roomVec;
 }
